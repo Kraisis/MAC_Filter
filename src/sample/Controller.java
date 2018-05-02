186547aeb88bb6550970991c87579ff6da299c2c
Alex King <gregory_king1995@hotmail.com>
2018-05-01 23:33:32 -0500

    Code Added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     Button confirm = new Button("Confirm");
+    TextField mac_address = new TextField();
+
+
 
 }
