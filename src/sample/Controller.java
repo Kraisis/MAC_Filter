f5cb398c7261650d3e2cd53f1500d69a03df3ecd
Alex King <gregory_king1995@hotmail.com>
2018-05-02 09:13:57 -0500

    Code Added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,45 @@
 package sample;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 
 public class Controller {
 
+    @FXML
     Button confirm = new Button("Confirm");
+    @FXML
     TextField mac_address = new TextField();
+    @FXML
+    Label label = new Label();
 
     //Comments added for dramatic effect
+
+
+    public void addMac(){
+        System.out.println("Added MAC???");
+
+        String mac = "";
+
+        mac = mac_address.getText();
+
+        System.out.println("The MAC Address entered was: " + mac);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
