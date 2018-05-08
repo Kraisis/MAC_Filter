cf15fe31e18cb0d5f04e65271b1a500e0bd0734e
Alex King <gregory_king1995@hotmail.com>
2018-05-08 01:11:42 -0500

    Re-did Windows

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -24,57 +26,65 @@ import java.util.regex.Pattern;
 
 public class Controller {
 
+    String username = "admin";
+    String password = "TSAATC2018";
+
+    ObservableList<String> deviceList = FXCollections.observableArrayList("Phone", "Computer", "Tablet");
+
+
+
     @FXML
-    Button confirm = new Button("Confirm");
+    Button confirm = new Button();
+    @FXML
+    Button nothing = new Button();
     @FXML
     TextField mac_address = new TextField();
+    @FXML
+    TextField name_tf = new TextField();
     @FXML
     Label label = new Label();
     @FXML
     Button ok = new Button();
+    @FXML
+    ChoiceBox device;
 
     //Comments added for dramatic effect
+    public void addMac() throws InterruptedException {
+
+        String mac = mac_address.getText();
+        String name = name_tf.getText();
+        String dev = (String)device.getValue();
 
 
-    public void addMac(){
-        System.out.println("Added MAC???");
-        String mac = "";
-        mac = mac_address.getText();
 
-        if(macValidate(mac)){
-
-            //if you didn't update the Path system variable to add the full directory path to the executable as above mentioned then doing this directly through code
-            System.setProperty("webdriver.gecko.driver", "geckodriver");
-            WebDriver driver = new FirefoxDriver();
-            driver.get("https://www.google.com");
-            System.out.println("Went to Google.com");
-            driver.manage().window().maximize();
-            driver.findElement(By.id("lst-ib")).sendKeys("Facebook");
-            driver.findElement(By.name("btnK")).click();
-
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-            }
-            driver.quit();
+        if(mac.equalsIgnoreCase("") && name.equalsIgnoreCase("")){
+            //Alert Box when nothing is entered
+            nothingEntered();
+            System.out.println("Nothing entered");
 
         }else{
-            System.out.println("Incorrect MAC");
+            if(macValidate(mac)){
+                //if you didn't update the Path system variable to add the full directory path to the executable as above mentioned then doing this directly through code
+                System.setProperty("webdriver.gecko.driver", "geckodriver");
+                WebDriver driver = new FirefoxDriver();
 
-            display();
+                if(login(driver)){
+                    System.out.println("Login Went Well");
+
+                }else{
+                    System.out.println("Error, could not login");
+                }
+
+                Thread.sleep(5000);
+                System.out.println("Successfully Terminated");
+                driver.quit();
+            }else{
+                System.out.println("Incorrect MAC");
+                display();
+            }
         }
 
 
-
-
-
-    }
-
-    public boolean validate(String mac) {
-        System.out.println("The MAC Address entered was: " + mac);
-        Pattern p = Pattern.compile("^([a-fA-F0-9][:-]){5}[a-fA-F0-9][:-]$");
-        Matcher m = p.matcher(mac);
-        return m.find();
     }
 
     public boolean macValidate(String mac) {
@@ -104,15 +114,53 @@ public class Controller {
     private void closeButtonAction(){
         Stage stage = (Stage) ok.getScene().getWindow();
         stage.close();
+    }
 
+    @FXML
+    private void closeButton(){
+        Stage stage = (Stage) nothing.getScene().getWindow();
+        stage.close();
+    }
+
+    public boolean login(WebDriver driver) throws InterruptedException {
+
+        try{
+//            driver.get("192.168.0.1/?wifi_mac");
+//            driver.findElement(By.id("UserName")).sendKeys(username);
+//            driver.findElement(By.id("Password")).sendKeys(password);
+//            driver.findElement(By.className("submitBtn")).click();
+            System.out.println("Login Successful");
+            Thread.sleep(1000);
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+
+    }
+
+    public void nothingEntered(){
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("nothing.fxml"));
+        } catch (IOException e) {
+            System.out.println("Couldn't Load FXML Document");
+        }
+        stage.setTitle("Required Fields");
+        stage.setScene(new Scene(root, 270, 85));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(confirm.getScene().getWindow());
+        stage.showAndWait();
     }
 
 
 
-
-
-
-
+    //This function crashes almost everything
+//    @FXML
+//    private void initialize(){
+//        device.setValue("Phone");
+//        device.setItems(deviceList);
+//    }
 
 
 
